85d4923ea05a1c7688a8af780fed85b766f45167
Prosanto Bhakta <proshantbhakta@gmail.com>
2026-04-02 20:36:28 +0600

    halfway to the course

--- main.go
+++ main.go
@@ -1,34 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/proshant45/femProject/internal/app"
+	"github.com/proshant45/femProject/internal/routes"
 )
 
 func main() {
+
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
+	defer app.DB.Close()
 
-	app.Logger.Println("Application started successfully")
-
-	http.HandleFunc("/health", HealthCheck)
+	r := routes.SetupRoutes(app)
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
+	app.Logger.Printf("we are running on port %d\n", port)
 	err = server.ListenAndServe()
 	if err != nil {
+
 		app.Logger.Fatal(err)
 	}
-}
-
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Status is available\n")
 }
